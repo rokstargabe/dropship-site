e9de31d1b713a972495ed3e93b3fa623094ba479
rokstargabe <gabe1239@gmail.com>
2025-08-10 19:22:45 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+export default nextConfig;
